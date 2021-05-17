d41123ea26bbfc0ca33f7dba1d788bee8d316c9c
haojie <365435306@qq.com>
2021-05-17 21:42:22 +0800

    springboot-demo

---
+++ src/main/java/com/itheima/config/JdbcConfig.java
@@ -0,0 +1,51 @@
+package com.itheima.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@PropertySource("classpath:application.properties")
+//@EnableConfigurationProperties(JdbcProperties.class)
+public class JdbcConfig {
+/*
+    @Value("${jdbc.url}")
+    String url;
+    @Value("${jdbc.driverClassName}")
+    String driverClassName;
+    @Value("${jdbc.username}")
+    String username;
+    @Value("${jdbc.password}")
+    String password;
+
+    @Bean
+    public DataSource dataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        return druidDataSource;
+    }*/
+/*
+    @Bean
+    public DataSource dataSource(JdbcProperties jdbcProperties){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(jdbcProperties.getDriverClassName());
+        druidDataSource.setUrl(jdbcProperties.getUrl());
+        druidDataSource.setUsername(jdbcProperties.getUsername());
+        druidDataSource.setPassword(jdbcProperties.getPassword());
+        return druidDataSource;
+    }*/
+@Bean
+@ConfigurationProperties(prefix = "jdbc")
+public DataSource dataSource(){
+    return new DruidDataSource();
+}
+}
---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,12 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String birthday;
+}
